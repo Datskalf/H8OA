2642da0e8bec96973115224aa410c36866bd216f
Sondre <datskalf@outlook.com>
2023-10-04 11:14:05 +0100

    Update main.py
    Now returns an instance of the flask.Response object, rather than just the string message. Allows the use of status codes

--- RPi-controller/main.py
+++ RPi-controller/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, Response
 
 import Lib.H8OA as H8OA
 
@@ -8,17 +8,17 @@ controller = H8OA.H8OA()
 app = Flask(__name__)
 
 @app.route("/control")
-def control() -> str:
+def control() -> Flask:
     return render_template("control.html")
 
 @app.route("/instruction", methods=["POST"])
-def handle_instruction() -> str:
+def handle_instruction() -> Response:
     if request.is_json:
         data = request.json
         controller.handle_command(data)
-        return "Accepted"
-    return "Incorrect Content-Type"
+        return Response("Accepted", status=200)
+    return Response("Incorrect Content-Type", status=406)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) 
