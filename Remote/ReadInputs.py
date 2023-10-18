ecee4e8830e13943d37b00974c8c87d8a07d2ab9
Sondre <datskalf@outlook.com>
2023-10-18 15:48:21 +0100

    Update ReadInputs.py

--- Remote/ReadInputs.py
+++ Remote/ReadInputs.py
@@ -1,24 +1,25 @@
 import signal
 from xbox360controller import Xbox360Controller
 
-Xbox360Controller.get_available()
 
 def on_button_pressed(button):
-    print(f"Button {button} was pressed")
+    print('Button {0} was pressed'.format(button.name))
+
 
 def on_button_released(button):
-    print(f"Button {button} was released")
+    print('Button {0} was released'.format(button.name))
+
 
 def on_axis_moved(axis):
-    print(f"Axis {axis.name} moved to ({axis.x}, {axis.y})")
+    print('Axis {0} moved to {1} {2}'.format(axis.name, axis.x, axis.y))
 
 try:
     with Xbox360Controller(0, axis_threshold=0.2) as controller:
+        # Button A events
         controller.button_a.when_pressed = on_button_pressed
-        controller.button_b.when_pressed = on_button_pressed
-        controller.button_x.when_pressed = on_button_pressed
-        controller.button_y.when_pressed = on_button_pressed
-        
+        controller.button_a.when_released = on_button_released
+
+        # Left and right axis move event
         controller.axis_l.when_moved = on_axis_moved
         controller.axis_r.when_moved = on_axis_moved
 
